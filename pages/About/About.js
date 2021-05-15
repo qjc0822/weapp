a92b41d47e18cce696a5cf7fbd90f776c302f6f8
qjc0822 <qjc0822@126.com>
2021-05-15 15:38:42 +0800

    init

---
+++ pages/About/About.js
@@ -0,0 +1,80 @@
+// pages/About/About.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    timestamp: "2019-03-19 21:27:41",
+    content: "捡到XX年级，XX专业，学生号为XXXXXX的校园一卡通。",
+    num: 3,
+  },
+
+  //  跳转至发布页面
+  // Publish: function () {
+  //   wx.navigateTo({
+  //     url: '../commit/commit'
+  //   })
+  // },
+
+  //删除按钮
+  // Delete:function(){
+
+  // }
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/wxml/index.js
@@ -0,0 +1,97 @@
+// pages/wxml/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataArray:[
+      {id:1,info:"xiba",chat:4},
+      {id:2,info:"who losted a laptop in classrom 308",chat:2},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:4,info:"ready!!!",chat:1},
+     
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    ],
+    showmenu:false
+  },
+  ididid(){
+    wx.navigateTo({
+      url: '../index/index',
+    })
+  },
+  showmenu(){
+    this.setData({ showmenu:true })
+  },
+  onclose(){
+    this.setData({ showmenu:false })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
