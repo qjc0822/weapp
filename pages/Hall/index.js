8a7951beaa93c5864b6b051d90d99f888138b41a
qjc0822 <qjc0822@126.com>
2021-05-18 16:18:39 +0800

    update3

--- pages/Hall/index.js
+++ pages/Hall/index.js
@@ -5,27 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-    dataArray:[
-      {id:1,info:"xiba",chat:4},
-      {id:2,info:"who losted a laptop in classrom 308",chat:2},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:4,info:"ready!!!",chat:1},
+    dataArray:{},
+    // dataArray:[
+    //   {id:1,info:"xiba",chat:4},
+    //   {id:2,info:"who losted a laptop in classrom 308",chat:2},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:4,info:"ready!!!",chat:1},
      
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-      {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
-    ],
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    //   {id:3,info:"this is a pure test about this system and it is long enough",chat:3},
+    // ],
+
     showmenu:false
   },
   ididid(){
@@ -48,7 +50,39 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var self=this;
+    wx.request({
+      url: getApp().globalData.server + '', //仅为示例，并非真实的接口地址
+      data: {
+      },
+      method:"POST",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+        if(res.data.error_code!=0){
+          wx.showModal({
+            title: 'SomethingWentWrong',
+            content: "gg"+res.data.msg,
+            showCancel: false,
+            success:function(res){
+              if(res.confirm){
+                console.log("yes")
+              }
+              else if(res.cancel){
+                console.log("no")
+              }
+            },
+          })
+        }
+        else if(res.data.error_code==0){
+          that.setData({
+            dataArray: res.data.data
+          })
+        }
+      }
+    })
   },
 
   /**
